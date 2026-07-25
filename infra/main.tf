65433bd6073028d440cd94095c83c56f9dc39b9d
anish0099 <kanish92729@gmail.com>
2026-07-25 10:08:45 +0530

    Creating the state file for terraform

---
+++ infra/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "my-app-tfstate-911167889594" # Globally unique bucket name
+    key    = "production/terraform.tfstate"
+    region = "ap-south-1"                  # Set to your AWS region
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"                    # Set to your AWS region
+}
